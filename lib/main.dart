6d902348909f823c7d9b991cfda326df213e0603
ahmedragab333 <ahmedragab2332002@gmail.com>
2023-09-22 05:24:33 +0300

    app init

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) { 
     return MaterialApp(
-
-
-
-
-
-
       debugShowCheckedModeBanner: false,
       initialRoute: HomePage.routeName,
       routes: {
